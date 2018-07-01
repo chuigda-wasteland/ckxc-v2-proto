f2af8a8c64cf02b6c2d80c4a396d788ed1f6180e
ice1000 <ice1000kotlin@foxmail.com>
2018-07-01 13:15:51 +0800

    Fix typo, avoid using built-in `error` function

--- fuckyou/src/cn/ckxily/ckxc/err/err.kt
+++ fuckyou/src/cn/ckxily/ckxc/err/err.kt
@@ -4,10 +4,12 @@ import java.lang.System.exit
 
 fun error(desc: String): Nothing {
 	println("Error: $desc")
+	@Suppress("UNREACHABLE_CODE", "CAST_NEVER_SUCCEEDS")
 	return exit(1) as Nothing
 }
 
 fun assertionFailed(desc: String): Nothing {
 	println("Assertion failed: $desc")
+	@Suppress("UNREACHABLE_CODE", "CAST_NEVER_SUCCEEDS")
 	return exit(-1) as Nothing
 }
