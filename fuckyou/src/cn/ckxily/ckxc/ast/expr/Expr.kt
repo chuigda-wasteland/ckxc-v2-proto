b4f9c399e702a8038e7226345ff0284c98cee759
ICEY <crazycz.cc@ckx-ily.cn>
2018-07-21 10:17:59 +0800

    增加运算符优先级选项

--- fuckyou/src/cn/ckxily/ckxc/ast/expr/Expr.kt
+++ fuckyou/src/cn/ckxily/ckxc/ast/expr/Expr.kt
@@ -3,7 +3,7 @@ package cn.ckxily.ckxc.ast.expr
 import cn.ckxily.ckxc.codegen.ASTConsumer
 import cn.ckxily.ckxc.ast.decl.Decl
 
-enum class ExprId(val str: String) {
+enum class ExprId(val desc: String) {
 	DeclRefExpr("Declaration reference expression"),
 	MemberAccessExpr("Member access expression"),
 	IntegralLiteralExpr("Integral literal"),
@@ -13,41 +13,41 @@ enum class ExprId(val str: String) {
 	ShortcutBinaryExpr("Shortcut binary expression")
 }
 
-enum class UnaryOpCode(val str: String) {
-	PreInc("Pre increment"),
-	PreDec("Pre decrement"),
-	Positive("Unary positive"),
-	Negative("Unary negative"),
-	Not("Logical not"),
-	BitwiseRevert("Bitwise revert"),
-	AddressOf("Taking address"),
-	DePointer("Dereferencing pointer")
+enum class UnaryOpCode(val str: String, val desc: String) {
+	PreInc("++", "Pre increment"),
+	PreDec("--", "Pre decrement"),
+	Positive("+", "Unary positive"),
+	Negative("-", "Unary negative"),
+	Not("!", "Logical not"),
+	BitwiseRevert("!", "Bitwise revert"),
+	AddressOf("&", "Taking address"),
+	DePointer("*", "Dereferencing pointer")
 }
 
-enum class BinaryOpCode(val str: String, val prec: Int) {
-	Assign("Assignment", 0),
-	LogicAnd("Logical and", 10),
-	LogicOr("Logical or", 10),
-	Less("Less than", 20),
-	Greater("Greater than", 20),
-	Equal("Equal to", 20),
-	LEQ("Less than or equal to", 20),
-	GEQ("Greater than or equal to", 20),
-	NEQ("Not equal to", 20),
-	BitwiseAnd("Bitwise and", 30),
-	BitwiseOr("Bitwise or", 30),
-	BitwiseXor("Bitwise Xor", 30),
-	Add("Add", 40),
-	Sub("Substract", 40),
-	Mul("Multiply", 50),
-	Div("Divide", 50),
-	Mod("Mod", 50),
-	UnaryOperation("Unary operations", 110)
+enum class BinaryOpCode(val str: String, val desc: String, val prec: Int) {
+	Assign("=", "Assignment", 0),
+	LogicAnd("&&", "Logical and", 10),
+	LogicOr("||", "Logical or", 10),
+	Less("<", "Less than", 20),
+	Greater(">", "Greater than", 20),
+	Equal("==", "Equal to", 20),
+	LEQ("<=", "Less than or equal to", 20),
+	GEQ(">=", "Greater than or equal to", 20),
+	NEQ("!=", "Not equal to", 20),
+	BitwiseAnd("&", "Bitwise and", 30),
+	BitwiseOr("|", "Bitwise or", 30),
+	BitwiseXor("!", "Bitwise Xor", 30),
+	Add("+", "Add", 40),
+	Sub("-", "Substract", 40),
+	Mul("*", "Multiply", 50),
+	Div("/", "Divide", 50),
+	Mod("%", "Mod", 50),
+	UnaryOperation("", "Unary operations", 110)
 }
 
-val ExprId.description get() = str
+val ExprId.description get() = desc
 
-val UnaryOpCode.description get() = str
+val UnaryOpCode.description get() = desc
 
 abstract class Expr(val exprId: ExprId) {
 	abstract fun accept(astConsumer: ASTConsumer): Any?
